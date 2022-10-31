a600a5f0e24cee6a08eb4de47985191b3bd8b005
Brabada <freemindobject@gmail.com>
2022-10-31 22:14:10 +0300

    Remove get_or_404 from non-view file

--- scripts.py
+++ scripts.py
@@ -5,24 +5,27 @@ import sys
 import django.core.exceptions
 from datacenter.models import Schoolkid, Mark, Commendation, Lesson, \
     Chastisement, Subject
-from django.http import Http404
-from django.shortcuts import get_object_or_404
 
 
 def get_schoolkid(kid_name):
     if not kid_name:
         raise ValueError("Строка с ФИО не должна быть пустой.")
-    schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
+    except django.core.exceptions.ObjectDoesNotExist:
+        raise django.core.exceptions.ObjectDoesNotExist(
+            f"Ошибка! По запросу {kid_name} ничего не найдено.\nВозможно вы "
+            f"допустили опечатку.\nФормат записи ФИО или ФИ."
+        )
     return schoolkid
 
 
 def fix_marks(kid_name):
     schoolkid = get_schoolkid(kid_name)
     good_mark_limit = 4
-    kid_marks = Mark.objects.filter(
+    Mark.objects.filter(
         schoolkid=schoolkid,
-        points__lt=good_mark_limit
-    ).update(points=5)
+        points__lt=good_mark_limit).update(points=5)
 
 
 def remove_chastisements(kid_name):
@@ -67,23 +70,24 @@ def create_commendation(kid_name, subject):
     random_commendation = random.choice(commendations)
     schoolkid = get_schoolkid(kid_name)
     try:
-        subject_check = get_object_or_404(Subject, title=subject,
-                                          year_of_study=schoolkid.year_of_study)
-    except Http404:
-        logging.error(f"По предмету {subject} нет результатов.\n"
-                      f"Возможно вы допустили опечатку в названии предмета.")
-        sys.exit(1)
+        Subject.objects.get(title=subject,
+                            year_of_study=schoolkid.year_of_study)
+    except django.core.exceptions.ObjectDoesNotExist:
+        raise django.core.exceptions.ObjectDoesNotExist(
+            f"По предмету {subject} нет результатов."
+            f"Возможно вы допустили опечатку в названии предмета.")
     kid_lessons = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
                                         group_letter=schoolkid.group_letter,
                                         subject__title=subject
                                         ).order_by('-date')
     for lesson in kid_lessons:
         try:
-            kid_commendation = get_object_or_404(Commendation,
-                                                 schoolkid=schoolkid,
-                                                 subject__title=subject,
-                                                 created=lesson.date)
-        except Http404:
+            kid_commendation = Commendation.objects.get(
+                schoolkid=schoolkid,
+                subject__title=subject,
+                created=lesson.date
+            )
+        except django.core.exceptions.ObjectDoesNotExist:
             kid_commendation = None
         if not kid_commendation:
             Commendation.objects.create(text=random_commendation,
@@ -103,10 +107,8 @@ def fix_all(kid_name, subject):
         logging.error(f"Ошибка! По запросу {kid_name} найдено несколько "
                       "результатов. Уточните имя.")
         sys.exit(1)
-    except django.core.exceptions.ObjectDoesNotExist:
-        logging.error(f"Ошибка! По запросу {kid_name} никого не найдено.\n"
-                      "Возможно вы допустили опечатку.\n"
-                      "Название должно быть в формате ФИО или ФИ.")
+    except django.core.exceptions.ObjectDoesNotExist as ex:
+        logging.error(ex.args)
         sys.exit(1)
     except ValueError as val_err:
         logging.error(val_err.args)
